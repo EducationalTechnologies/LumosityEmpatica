80ba23cf00ba5079e1fa71b08e39c14ebf848e00
Tanja25 <34271543+Tanja25@users.noreply.github.com>
2021-01-27 22:39:09 +0100

    analysis.py with logistic regression
    create the folder "confusion_matrix_and_ROC_Curve" before you run it

--- analysis.py
+++ analysis.py
@@ -19,8 +19,8 @@ target_classes = ['mistake']
 tensor_data, annotations, attributes = data_helper.get_data_from_files(train_folder, ignore_files=ignore_files, res_rate=25,
                                                            to_exclude=to_exclude)
 
-#print("Shape of the tensor_data is: " + str(np.shape(tensor_data)))
-#print("Shape of the annotation is: " + str(np.shape(annotations)))
+print("Shape of the tensor_data is: " + str(np.shape(tensor_data)))
+print("Shape of the annotation is: " + str(np.shape(annotations)))
 
 attributes = ['BVP', 'GSR', 'HRV', 'IBI', 'TMP']
 
@@ -46,40 +46,7 @@ tabular_representation['IBI_mean'] = np.mean(tensor_data[:,:,3],axis=1)
 # TMP
 # matrix TMP tensor_data[:,:,4]
 tabular_representation['TMP_mean'] = np.mean(tensor_data[:,:,4],axis=1)
-train_folder = "manual_sessions/lumosity-dataset"
-ignore_files = []
-to_exclude = ['ACC','OenName','RecordingID', 'ApplicationName'] #exclude the following attributes in generating the Tensor
-target_classes = ['mistake']
-tensor_data, annotations, attributes = data_helper.get_data_from_files(train_folder, ignore_files=ignore_files, res_rate=25,
-                                                           to_exclude=to_exclude)
 
-print("Shape of the tensor_data is: " + str(np.shape(tensor_data)))
-print("Shape of the annotation is: " + str(np.shape(annotations)))
-
-attributes = ['BVP', 'GSR', 'HRV', 'IBI', 'TMP']
-
-tabular_representation = annotations[['recordingID', 'mistake', 'end', 'start', 'duration']].copy()
-
-# BVP
-# matrix BVP tensor_data[:,:,0]
-tabular_representation['BVP_std'] = np.mean(tensor_data[:,:,0],axis=1)
-#print(tabular_representation['BVP_std'])
-
-# GSP
-# matrix GSR tensor_data[:,:,1]
-tabular_representation['GSR_mean'] = np.mean(tensor_data[:,:,1],axis=1)
-
-# HRV
-# matrix HRV tensor_data[:,:,2]
-tabular_representation['HRV_mean'] = np.mean(tensor_data[:,:,2],axis=1)
-
-# IBI
-# matrix IBI tensor_data[:,:,3]
-tabular_representation['IBI_mean'] = np.mean(tensor_data[:,:,3],axis=1)
-
-# TMP
-# matrix TMP tensor_data[:,:,4]
-tabular_representation['TMP_mean'] = np.mean(tensor_data[:,:,4],axis=1)
 
 #############################################################################################################
 #Tanjas code changing
