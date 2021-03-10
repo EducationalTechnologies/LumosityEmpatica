ac7aacba1dcf9a741dd16522e5d6365a7e504bcb
Tanja25 <34271543+Tanja25@users.noreply.github.com>
2021-03-10 19:13:31 +0100

    HMM with MultivariateGaussianDistribution

--- analysis_hmm.py
+++ analysis_hmm.py
@@ -1,7 +1,8 @@
 import numpy as np
 import data_helper
 import os
-from hmmlearn import hmm
+import pomegranate as pg
+
 
 if __name__ == "__main__":
     data_folder = "manual_sessions/lumosity-dataset"
@@ -22,14 +23,95 @@ if __name__ == "__main__":
     X = tensor_data
     y = annotations.reset_index(drop=True)
 
+    print(X.shape, y.shape)
     # Let's consider only GSR
     df = annotations[['recordingID', 'mistake', 'end', 'start', 'duration']].copy()
     df['gsr'] = np.mean(tensor_data[:, :, 1], axis=1)
+    df['bvp'] = np.mean(tensor_data[:, :, 2], axis=1)
+    df['ibi'] = np.mean(tensor_data[:, :, 3], axis=1)
+    df['tmp'] = np.mean(tensor_data[:, :, 4], axis=1)
 
-    # Initializing 
-    # X = np.concatenate([X1, X2])
-    # lengths = [len(X1), len(X2)]
-    # hmm_model = hmm.GaussianHMM(n_components=3, covariance_type="full", n_iter=100)
-    # hmm_model.fit(X, lengths)
 
-    #y_pred = remodel.predict(X)
+    uniqueId = df["recordingID"].unique()
+    observations = []
+    mistakes = []
+    #mistakes_partcp = []
+    for id_ in uniqueId:
+        df_part = df.loc[df['recordingID'] == id_]
+        mistake = df_part['mistake']
+        mistake_list = []
+        participant = [df_part['gsr'], df_part['bvp'], df_part['ibi'], df_part['tmp']]
+        values = []
+        for column in participant:
+            columns = []
+            for value in column:
+                columns.append(value)
+            values.append(columns)
+        observations.append(values)
+        for value in mistake:
+            mistakes.append(value)                  # mistakes = [mistakes_1_to_1927]
+
+    end_list = []
+    for partc in observations:
+        for a in range(len(partc[0])):
+            temp = []
+            for b in range(len(observations[0])):
+                temp.append(partc[b][a])
+            end_list.append(temp)
+    observations = end_list                 #[[GSR_1,BVP_1,IBI_1,TMP_1],...[GSR_1997,BVP_1927,IBI_1991,TMP_1927]]
+
+    X = np.array([np.array(x) for x in observations], dtype=object)
+    labels = np.array(mistakes)
+
+    X = np.expand_dims(X, axis=0)
+    labels = np.expand_dims(mistakes, axis=0)
+
+    print("Shape of X:", X.shape)
+    print("Shape of labels:", labels.shape)
+
+
+    model = pg.HiddenMarkovModel.from_samples(pg.MultivariateGaussianDistribution,
+                                            n_components=len(np.unique(labels)),
+                                            X=X,
+                                            labels=labels,
+                                            algorithm='labeled')
+
+    test = np.array([[[0.1370942, 65.08173, 0.9219177, 30.369999999999994],
+                    [0.14720542857142857, 65.08173, 0.9219177, 30.369999999999994],
+                    [0.1370942, 76.64283057142856, 0.7942332199999999, 30.369999999999994]]])
+    test_y = np.array([[1,0,0]])
+
+    # print("viterbi:",model.viterbi(test))
+    # print("maximum_a_posteriori:", model.maximum_a_posteriori(test))
+    # print("predict_proba:", model.predict_proba(test))
+    print("Dense transition matrix:", model.dense_transition_matrix())
+    print("Emission and transition matrix:", model.forward_backward(test))
+    print("Number of states:", model.state_count())
+    print("Log probability:", model.log_probability(test))
+    print("Predicted test:", model.predict(test))
+    print("Score:", model.score(test, test_y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
